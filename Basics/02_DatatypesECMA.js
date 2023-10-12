c6b465d011219962cba5c031dba59d2fe0aac63b
amitknows <114345326+amitknows@users.noreply.github.com>
2023-10-12 13:42:23 +0000

    operations

--- Basics/02_DatatypesECMA.js
+++ Basics/02_DatatypesECMA.js
@@ -1,1 +1,25 @@
+"use strict" ; // treat all JS Code as newer version
+
+
+//alert ("amit") // we are useing nodejs, not browser
 console.log("Amit")
+
+let name = "Amit"
+let age = 10
+let isLoggedIn = false
+
+
+
+//number => 2 to powe 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefiend => 
+// symbol => unique
+
+
+// object
+
+console.log (typeof true)
+console.log (typeof undefined)
---
+++ Basics/03_Conversions.js
@@ -0,0 +1,71 @@
+/*   CONVERTING STRING TO NUMBER */
+
+let score = "33"
+let score1 = "amit"
+let score2= ""
+
+
+let scoreNum = Number(score)
+let scoreNum1 = Number(score1)
+let scoreNum2 = Number(score2)
+
+// console.log( scoreNum)
+// console.log( scoreNum1)
+// console.log( scoreNum2)
+
+// console.log(typeof scoreNum)
+// console.log(typeof scoreNum1)
+// console.log(typeof scoreNum2)
+
+
+/*   CONVERTING NUMBER TO STRING */
+
+
+let num = 11
+  
+let NumString = String(num)
+
+console.log(NumString)
+console.log(typeof NumString)
+
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
