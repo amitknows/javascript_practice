783356349ff9e417e24c5d608f652581314455a6
amitknows <114345326+amitknows@users.noreply.github.com>
2023-10-14 11:56:33 +0000

    Strings

---
+++ Basics/05_Strings.js
@@ -0,0 +1,33 @@
+const name = "amit"
+const repoCount = 50
+
+//console.log(name + repoCount + "Value ") // outdate style
+
+//new modern style
+//console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`) // it is called string interpolation
+
+const gameName = new String('amitpandey')
+
+// console.log(gameName[1]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("t"));
+
+const newString = gameName.substring(0,2)
+//console.log(newString)
+
+const anotherString = gameName.slice(-8,4)
+//console.log(anotherString)
+
+ const newStringOne = "                amit        "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+
+const url ="https://amit.com/amit%20pandey"
+
+console.log(url.replace("%20" , "-"))
--- Basics/DatatypesSummery.js
+++ Basics/DatatypesSummery.js
@@ -32,6 +32,29 @@ const myFunction = function(){
    // console.log("Hello world");
 }
 
-console.log(typeof bigNumber);
+//console.log(typeof bigNumber);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// Stack (Primitive) , Heap (Non-Primitive)
+
+let myYoutubeName = "amitpandeydotcom"
+
+let anotherName = myYoutubeName
+anotherName = "chaiaurcode"
+
+console.log(myYoutubeName)
+console.log(anotherName)
+
+let userOne = {
+    email: "user@google.com",
+    upi : "user@ybl",
+
+}
+
+let userTwo = userOne
+userTwo.email = "amit@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
